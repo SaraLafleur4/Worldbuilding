6ec7c0f1ee370444fbd5bb1cd7a8f7cba7793bd2
Xiantas <mat.moulat@gmail.com>
2025-01-21 20:32:26 +0100

    First fix

--- Assets/Scripts/Props/Trees/Forest.cs
+++ Assets/Scripts/Props/Trees/Forest.cs
@@ -12,8 +12,8 @@ public class Forest : MonoBehaviour
 
     [Header("Forest Settings")]
     public int numberOfOriginalTrees = 10;
-    public Vector2 planePos = new Vector2(250, 250);
-    public Vector2 planeSize = new Vector2(10, 10);
+    public Vector2 planePos = new Vector2(10, 10);
+    public Vector2 planeSize = new Vector2(500, 500);
 
     private List<AiTree> originalTrees = new List<AiTree>();
     public LSystem lSystem;
@@ -21,13 +21,10 @@ public class Forest : MonoBehaviour
 
     public void GenerateForest()
     {
-        // Clear existing forest
         ClearForest();
 
-        // Generate the original trees with the LSystem algorithm
-        for (int i = 0; i < numberOfOriginalTrees; i++)
-        {
-            GenerateSingleOriginalTree(i + 1); // treeNb should start at 1, not 0
+        for (int i = 0; i < numberOfOriginalTrees; i++) {
+            GenerateSingleOriginalTree(i + 1);
         }
 
         foreach (var tree in originalTrees)
@@ -35,30 +32,24 @@ public class Forest : MonoBehaviour
             // Parent the original tree's components under the new "OriginalTreeX" GameObject
             GameObject originalParent = new GameObject("OriginalTree" + (originalTrees.IndexOf(tree) + 1));
             foreach (var obj in tree.GetTreeMesh())
-            {
                 obj.transform.SetParent(originalParent.transform);
-            }
 
-            // Add the original tree to the tree list
             parentTrees.Add(originalParent);
 
-            // Duplicate the original tree 20 times
             for (int i = 0; i < 20; i++)
-            {
                 GenerateDuplicateTreeFromOriginal(tree.GetTreeMesh(), originalTrees.IndexOf(tree) + 1, i + 1);
-            }
         }
     }
 
     private void GenerateSingleOriginalTree(int treeNb)
     {
-        // L-System settings
         lSystem.SetupLSystemForSingleTree();
 
-        // Tree generation
         AiTree tree = gameObject.AddComponent<AiTree>();
         tree.SetMaterials(branchMaterial, leafMaterial);
-        tree.Generate(new Vector2(planeSize.x + planePos.y, planeSize.y + planePos.x),
+        tree.Generate(
+            planePos,
+            planeSize,
             lSystem.getCurrentString(),
             lSystem.length,
             lSystem.angle,
@@ -72,56 +63,40 @@ public class Forest : MonoBehaviour
         List<GameObject> newTreeComponents = new List<GameObject>();
 
         var newTreePosition = new Vector3(
-            Random.Range(0, planeSize.x + planePos.y),
+            planePos.x + Random.Range(0, planeSize.x),
             0,
-            Random.Range(0, planeSize.y + planePos.x)
+            planePos.y + Random.Range(0, planeSize.y)
         );
+        if (Terrain.activeTerrain != null) newTreePosition.y = Terrain.activeTerrain.SampleHeight(newTreePosition);
         var newTreeRotation = new Vector3(
             0,
             Random.Range(0, 360),
             0
         );
 
-        // Duplicate the branches and leaves
-        // Set them to the new position and new rotation
-        // Add them to the tree components list
-        foreach (var obj in originalTreeMesh)
-        {
+        foreach (var obj in originalTreeMesh) {
             GameObject duplicatedObj = Instantiate(obj);
             duplicatedObj.transform.position = newTreePosition;
             duplicatedObj.transform.eulerAngles = newTreeRotation;
             newTreeComponents.Add(duplicatedObj);
         }
 
-        // Parent the duplicated tree under the new "OriginalTreeX-DuplicateY" GameObject
         GameObject duplicateParent = new GameObject("OriginalTree" + originalTreeNb + "-Duplicate" + duplicateTreeNb);
         foreach (var obj in newTreeComponents)
-        {
             obj.transform.SetParent(duplicateParent.transform); // Set the parent to the duplicate tree parent
-        }
 
-        // Add the new tree to the tree list
         parentTrees.Add(duplicateParent);
     }
 
     private void ClearForest()
     {
-        // Destroy original AiTrees
         if (originalTrees != null)
         {
-            foreach (var tree in originalTrees)
-            {
-                tree.DestroyTree();
-            }
+            foreach (var tree in originalTrees) tree.DestroyTree();
             originalTrees.Clear();
         }
-        // Destroy GameObjects for all trees
-        if (parentTrees != null)
-        {
-            foreach (var tree in parentTrees)
-            {
-                Destroy(tree);
-            }
+        if (parentTrees != null) {
+            foreach (var tree in parentTrees) Destroy(tree);
         }
     }
 }
